3e95ba3cea53be49ebe1abacae1189f6d7a4744c
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-12-15 11:41:24 -0500

    download images

--- OrcMathGUI/src/danielComponent/CatalogMaker.java
+++ OrcMathGUI/src/danielComponent/CatalogMaker.java
@@ -10,14 +10,28 @@ public class CatalogMaker
 	{
 		movies = new ArrayList<Movie>();
 		
-		movies.add(0,new Movie("The Terminator", "James Cameron", 1984));
+		movies.add(0, new Movie("The Terminator", "James Cameron", 1984));
 		movies.add(1, new Movie("The Titanic", "James Cameron", 1997));
 		
 	}
 	
 	public static void main(String[] args) 
 	{
-		
+		CatalogMaker maker = new CatalogMaker();
+		System.out.println(maker.getCsvContent());
 	}
-
+	
+	public String getCsvContent()
+	{
+		String data = "";
+		
+		for (Movie a : movies)
+		{
+			data += a + "\n";
+		} 
+		return data;
+	}
+	
+	
+	
 }
